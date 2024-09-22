f22c4ec036c39ac1da48e00f5940a73d992b75b4
Jake <jsmith181997@gmail.com>
2024-09-22 18:55:50 -0500

    update term prediction model to classifer

--- src/forestRegression.py
+++ src/forestRegression.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.ensemble import RandomForestRegressor  # Use RandomForestRegressor for continuous outputs
-from sklearn.metrics import root_mean_squared_error
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier  # RandomForestRegressor for continuous outputs, RandomForestClassifier for discrete outputs
+from sklearn.metrics import root_mean_squared_error, accuracy_score
 
 import joblib
 
@@ -16,11 +16,13 @@ def train_models():
     # Encode categorical inputs (loan type)
     label_encoder = LabelEncoder()
     data['loan_type_encoded'] = label_encoder.fit_transform(data['loan_type'])
+    
+
 
     # Split the data into features (X) and labels/targets (y)
     X = data[['income', 'credit_score', 'loan_amount', 'property_value', 'loan_type_encoded']]
-    y_loan_term = data['loan_term']  # Assuming this is continuous
-    y_interest_rate = data['interest_rate']  # Assuming this is continuous
+    y_loan_term = data['loan_term']  # Continous output
+    y_interest_rate = data['interest_rate']  # Discrete - 15 or 30 year term
 
     # Train-test split (80% train, 20% test)
     # 80% of the data is used to train the model and then its evaluated on 20% of it
@@ -34,7 +36,7 @@ def train_models():
     X_test = scaler.transform(X_test)
 
     # Create and train the model for loan term
-    model_loan_term = RandomForestRegressor(n_estimators=100, random_state=42)
+    model_loan_term = RandomForestClassifier(n_estimators=100, random_state=42)
     model_loan_term.fit(X_train, y_train_loan_term)
 
     # Create and train the model for interest rate
@@ -60,14 +62,14 @@ def train_models():
     print(results.head())
 
     # Evaluate the models (sqaured=False to display RMSE for easier comparison)
-    mse_loan_term = root_mean_squared_error(y_test_loan_term, y_pred_loan_term)
+    loan_term_accuracy = accuracy_score(y_test_loan_term, y_pred_loan_term)
     mse_interest_rate = root_mean_squared_error(y_test_interest_rate, y_pred_interest_rate)
 
-    print(f"Loan Term RMSE: {mse_loan_term:.2f}")
-    print(f"Interest Rate RMSE: {mse_interest_rate:.2f}")
+    print(f"Loan Term Accuracy: {loan_term_accuracy * 100:.2f}%")
+    print(f"Interest Rate Error: {mse_interest_rate:.2f}%")
 
 
-    # Finally, save the model to use later
+    # Save the model to use later
     joblib.dump(model_loan_term, "../resources/saved-models/loan_term.pkl")
     joblib.dump(model_interest_rate, "../resources/saved-models/interest_rate.pkl")
     joblib.dump(scaler, "../resources/saved-models/scaler.pkl")
