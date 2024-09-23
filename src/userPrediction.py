bf62610293bc5daa0a6fc12ae687450feab60104
Jake <jsmith181997@gmail.com>
2024-09-22 19:16:08 -0500

    format code

--- src/userPrediction.py
+++ src/userPrediction.py
@@ -1,11 +1,10 @@
-
 # Collect financial data from the user and return as a data object
 def collect_user_input():
     data = {
-    'income': int(input("Enter your income: $")),
-    'credit_score': int(input("Enter your credit score: ")),
-    'loan_amount': int(input("Enter loan amount: $")),
-    'property_value': int(input("Enter property value: $")),
-    'loan_type': 'fixed' 
+        "income": int(input("Enter your income: $")),
+        "credit_score": int(input("Enter your credit score: ")),
+        "loan_amount": int(input("Enter loan amount: $")),
+        "property_value": int(input("Enter property value: $")),
+        "loan_type": "fixed",
     }
     return data
