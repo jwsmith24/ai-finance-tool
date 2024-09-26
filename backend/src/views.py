89a82c85b604d55dc6e6de26d2f630cf62cc414d
jake <jsmith181997@gmail.com>
2024-09-26 11:07:29 -0500

    setup flask API

--- backend/src/views.py
+++ backend/src/views.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 
-
-@app.route("/fish", methods=["GET"])
-def fish():
-    return jsonify({"message": "you've reached the API"})
+@app.route('/test', methods=['POST'])
+def handle_data():
+    data = request.get_json()
+    response = {
+        'message': 'Data received by the python backend!',
+        'data': data
+    }
+    return jsonify(response)
 
 
 if __name__ == "__main__":
