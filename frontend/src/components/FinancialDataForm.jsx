53ef2b9e3292991fd94b6ca94f84904a6595a56f
jake <jsmith181997@gmail.com>
2024-09-27 14:12:39 -0500

    refactor inputs with map

--- frontend/src/components/FinancialDataForm.jsx
+++ frontend/src/components/FinancialDataForm.jsx
@@ -26,110 +26,74 @@ export default function FinancialDataForm() {
         e.preventDefault();
         console.log("Form data:", formState);
     };
+    const inputClass = 'w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500'
+
+    const formFields = [
+        {id: 1, label: 'First Name', type: 'text', name: 'firstName', placeHolder: 'First name'},
+        {
+            id: 2,
+            label: 'Income',
+            type: 'number',
+            name: 'income',
+            placeHolder: 'Annual income in dollars'
+        },
+        {
+            id: 3,
+            label: 'Credit Score',
+            type: 'number',
+            name: 'creditScore',
+            placeHolder: 'Three-digit credit score'
+        },
+        {
+            id: 4,
+            label: 'Available Cash',
+            type: 'number',
+            name: 'cash',
+            placeHolder: 'Liquid assets in dollars'
+        },
+        {
+            id: 5,
+            label: 'Property Value',
+            type: 'number',
+            name: 'propertyValue',
+            placeHolder: 'Property value in dollars'
+        },
+        {
+            id: 6,
+            label: 'Loan Amount',
+            type: 'number',
+            name: 'loanAmount',
+            placeHolder: 'Loan amount in dollars'
+        },
+        {
+            id: 7,
+            label: 'Interest Rate',
+            type: 'number',
+            name: 'interestRate',
+            placeHolder: '#.## %'
+        },
+    ]
 
     return (
-        <form id="finData" onSubmit={handleSubmit} className={"max-w-lg p-6 mx-auto bg-white shadow-lg rounded-lg"}>
+        <form id="finData" onSubmit={handleSubmit}
+              className={"max-w-lg p-6 mx-auto bg-white shadow-lg rounded-lg"}>
             <h2 className={"text-2xl font-bold mb-4 text-left"}>Financial Data</h2>
-            <div className={"mb-4"}>
-                <label htmlFor="name" className={"block text-sm font-medium text-gray-700 mb-2"}>Name: </label>
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    value={formState.name}
-                    onChange={handleChange}
-                    required
-                    className={"w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"}
-                    placeholder={"FirstName LastName"}
-                />
-            </div>
-            <div className={"mb-4"}>
-                <label htmlFor="income">Income: </label>
-                <input
-                    type="number"
-                    name="income"
-                    id="income"
-                    min={0}
-                    value={formState.income}
-                    onChange={handleChange}
-                    required
-                    className={"w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"}
-                    placeholder={"Annual income in dollars"}
-                />
-            </div>
-            <div className={"mb-4"}>
-                <label htmlFor="creditScore">Credit Score: </label>
-                <input
-                    type="number"
-                    name="creditScore"
-                    id="creditScore"
-                    value={formState.creditScore}
-                    onChange={handleChange}
-                    min="300"
-                    max="850"
-                    required
-                    className={"w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"}
-                    placeholder={"3-digit credit score"}
-                />
-            </div>
-            <div className={"mb-4"}>
-                <label htmlFor="cash">Available Cash:</label>
-                <input
-                    type="number"
-                    name="cash"
-                    id="cash"
-                    min="0"
-                    value={formState.cash}
-                    onChange={handleChange}
-                    required
-                    className={"w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"}
-                    placeholder={"Liquid assets in dollars"}
-                />
-            </div>
-            <div className={"mb-4"}>
-                <label htmlFor="propertyValue">Property Value: </label>
-                <input
-                    type="number"
-                    name="propertyValue"
-                    id="propertyValue"
-                    min={"0"}
-                    value={formState.propertyValue}
-                    onChange={handleChange}
-                    required
-                    className={"w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"}
-                    placeholder={"Property value in dollars"}
-                />
-            </div>
-            <div className={"mb-4"}>
-                <label htmlFor="loanAmount">Loan Amount: </label>
-                <input
-                    type="number"
-                    name="loanAmount"
-                    id="loanAmount"
-                    min={"0"}
-                    value={formState.loanAmount}
-                    onChange={handleChange}
-                    required
-                    className={"w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"}
-                    placeholder={"Loan amount in dollars"}
-                />
-            </div>
-            <div className={"mb-4"}>
-                <label htmlFor="interestRate">Interest Rate: </label>
-                <input
-                    type="number"
-                    name="interestRate"
-                    id="interestRate"
-                    min={"1"}
-                    max={"15"}
-                    step={"0.01"}
-                    value={formState.interestRate}
-                    onChange={handleChange}
-                    required
-                    className={"w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"}
-                    placeholder={"x.xx%"}
-                />
-            </div>
+
+            {formFields.map((field) => (
+                <div key={field.id} className={'mb-3'}>
+                    <label htmlFor={field.name}>{field.label}</label>
+                    <input
+                        type={field.type}
+                        name={field.name}
+                        id={field.id}
+                        value={formState[field.name]}
+                        onChange={handleChange}
+                        required
+                        className={inputClass}
+                    />
+                </div>
+            ))}
+
             <div className={"text-center"}>
                 <button
                     type="submit"
