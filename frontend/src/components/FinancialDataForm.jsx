605a7346e83df3dad9cede35c04e4a2eb839a8cb
jake <jsmith181997@gmail.com>
2024-09-26 21:54:41 -0500

    remove multiple stylesheets, integrate tailwind

--- frontend/src/components/FinancialDataForm.jsx
+++ frontend/src/components/FinancialDataForm.jsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import "../style/Forms.css"
+
 
 export default function FinancialDataForm() {
     // init form state
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,11 @@
-import "../style/Navbar.css";
-
 export default function Navbar() {
-  return (
-    <nav className="navbar">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Some Other Thing</li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Some Other Thing</li>
+            </ul>
+        </nav>
+    );
 }
